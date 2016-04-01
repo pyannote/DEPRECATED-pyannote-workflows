d83b80e1370d68c966637f8376147c3c9d1bbbed
Hervé BREDIN <bredin@limsi.fr>
2016-04-01 12:24:05 +0200

    feat: ArgMaxTagging propagation task

--- pyannote_workflows/tasks/propagation.py
+++ pyannote_workflows/tasks/propagation.py
@@ -29,3 +29,28 @@ class ConservativeDirectTagging(sciluigi.Task, AutoOutput):
 
         with self.out_put().open('w') as fp:
             pyannote.core.json.dump(tagged, fp)
+
+
+class ArgMaxTagging(sciluigi.Task, AutoOutput):
+
+    in_source = None
+    in_target = None
+
+    def run(self):
+
+        # source (with string labels)
+        with self.in_source().open('r') as fp:
+            source = pyannote.core.json.load(fp)
+
+        # target (with integer labels)
+        with self.in_target().open('r') as fp:
+            target = pyannote.core.json.load(fp)
+            target = target.anonymize_labels(generator='int')
+
+        tagging = pyannote.algorithms.tagging.ArgMaxTagger()
+        tagged = tagging(source, target)
+
+        tagged = tagged.anonymize_labels(generator='string')
+
+        with self.out_put().open('w') as fp:
+            pyannote.core.json.dump(tagged, fp)
